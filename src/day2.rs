438d6c4789323e5e7dfec44bc8b2fb53b918c6b2
Jade Misenas <cjmisenas@gmail.com>
2021-12-01 21:23:39 -0800

    Day 2 done

--- src/day2.rs
+++ src/day2.rs
@@ -5,20 +5,53 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 pub fn main() {
-    let lines = read_lines_as_int("./day2.input");
-    let answer1 = solve1(&lines);
-    let answer2 = solve2(&lines);
+    let lines = read_lines_as_str("./day2.input");
+    let parsed_lines = lines.iter().map(|l| l.split(" ").collect()).collect();
+    let answer1 = solve1(&parsed_lines);
+    let answer2 = solve2(&parsed_lines);
     println!("Day 2 answers");
     println!("Answer 1 {}", answer1);
     println!("Answer 2 {}", answer2);
 }
 
-fn solve1(lines: &[u32]) -> u32 {
-    0
+fn solve1(lines: &Vec<Vec<&str>>) -> u32 {
+    let mut horizontal = 0;
+    let mut vertical = 0;
+
+    for instructions in lines.iter() {
+        let dir = instructions[0];
+        let units = instructions[1].parse::<u32>().unwrap();
+
+        match dir {
+            "forward" => horizontal += units,
+            "down" => vertical += units,
+            _ => vertical -= units,
+        }
+    }
+
+    horizontal * vertical
 }
 
-fn solve2(lines: &[u32]) -> u32 {
-    0
+fn solve2(lines: &Vec<Vec<&str>>) -> u32 {
+    let mut horizontal = 0;
+    let mut vertical = 0;
+    let mut aim = 0;
+
+    for instructions in lines.iter() {
+        let dir = instructions[0];
+        let units = instructions[1].parse::<u32>().unwrap();
+
+        match dir {
+            "down" => aim += units,
+            "up" => aim -= units,
+            _ => {
+                horizontal += units;
+                vertical += aim * units;
+            }
+        }
+    }
+
+    horizontal * vertical
 }
 
 fn read_lines_as_str<P>(filename: P) -> Vec<String>
@@ -29,16 +62,5 @@ where
     let buf = io::BufReader::new(file);
     buf.lines()
         .map(|l| l.expect("Could not parse line"))
-        .collect()
-}
-
-fn read_lines_as_int<P>(filename: P) -> Vec<u32>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename).expect("no such file");
-    let buf = io::BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("Could not parse line").parse::<u32>().unwrap())
         .collect()
 }
