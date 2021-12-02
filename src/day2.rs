c2f9f4f93e0601e41f8e2c37e86b6555e05ef37a
Jade Misenas <cjmisenas@gmail.com>
2021-12-01 21:39:01 -0800

    Cleanup 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,12 +1,24 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+struct Instruction {
+    dir: String,
+    unit: u32,
+}
+
 pub fn main() {
     let lines = read_lines_as_str("./day2.input");
-    let parsed_lines = lines.iter().map(|l| l.split(" ").collect()).collect();
+    let parsed_lines = lines
+        .iter()
+        .map(|l| {
+            let ins = l.split(" ").collect::<Vec<&str>>();
+            Instruction {
+                dir: ins[0].to_string(),
+                unit: ins[1].parse::<u32>().unwrap(),
+            }
+        })
+        .collect::<Vec<Instruction>>();
     let answer1 = solve1(&parsed_lines);
     let answer2 = solve2(&parsed_lines);
     println!("Day 2 answers");
@@ -14,39 +26,33 @@ pub fn main() {
     println!("Answer 2 {}", answer2);
 }
 
-fn solve1(lines: &Vec<Vec<&str>>) -> u32 {
+fn solve1(lines: &Vec<Instruction>) -> u32 {
     let mut horizontal = 0;
     let mut vertical = 0;
 
-    for instructions in lines.iter() {
-        let dir = instructions[0];
-        let units = instructions[1].parse::<u32>().unwrap();
-
-        match dir {
-            "forward" => horizontal += units,
-            "down" => vertical += units,
-            _ => vertical -= units,
+    for ins in lines.iter() {
+        match ins.dir.as_str() {
+            "forward" => horizontal += ins.unit,
+            "down" => vertical += ins.unit,
+            _ => vertical -= ins.unit,
         }
     }
 
     horizontal * vertical
 }
 
-fn solve2(lines: &Vec<Vec<&str>>) -> u32 {
+fn solve2(lines: &Vec<Instruction>) -> u32 {
     let mut horizontal = 0;
     let mut vertical = 0;
     let mut aim = 0;
 
-    for instructions in lines.iter() {
-        let dir = instructions[0];
-        let units = instructions[1].parse::<u32>().unwrap();
-
-        match dir {
-            "down" => aim += units,
-            "up" => aim -= units,
+    for ins in lines.iter() {
+        match ins.dir.as_str() {
+            "down" => aim += ins.unit,
+            "up" => aim -= ins.unit,
             _ => {
-                horizontal += units;
-                vertical += aim * units;
+                horizontal += ins.unit;
+                vertical += aim * ins.unit;
             }
         }
     }
