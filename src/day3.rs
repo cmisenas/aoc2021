f3bb0df143976db75ab14e88df6fcd702008c907
Jade Misenas <cjmisenas@gmail.com>
2021-12-02 22:01:58 -0800

    Day 3 done

--- src/day3.rs
+++ src/day3.rs
@@ -30,7 +30,6 @@ fn solve1(lines: &Vec<String>) -> usize {
                 }
             });
     }
-    println!("{:?}", count);
 
     let mut gamma = "".to_owned();
     let mut epsilon = "".to_owned();
@@ -44,11 +43,58 @@ fn solve1(lines: &Vec<String>) -> usize {
             gamma.push_str("0");
         }
     }
-    return convert_to_decimal(gamma.to_string()) * convert_to_decimal(epsilon.to_string());
+    convert_to_decimal(gamma.to_string()) * convert_to_decimal(epsilon.to_string())
 }
 
 fn solve2(lines: &Vec<String>) -> usize {
-    0
+    let mut index = 0;
+    let mut temp_oxygen_rating = lines.clone();
+    let mut temp_co2_rating = lines.clone();
+    while index < lines[0].len() {
+        if temp_oxygen_rating.len() > 1 {
+            let mut temp_o2_rating_1s = vec![];
+            let mut temp_o2_rating_0s = vec![];
+            for line in temp_oxygen_rating.iter() {
+                if line.chars().nth(index).unwrap() == '1' {
+                    temp_o2_rating_1s.push(line.to_string());
+                } else {
+                    temp_o2_rating_0s.push(line.to_string());
+                }
+            }
+
+            // Most common value
+            if temp_o2_rating_1s.len() >= temp_o2_rating_0s.len() {
+                // Keep values with 1 or most common value
+                temp_oxygen_rating = temp_o2_rating_1s;
+            } else {
+                temp_oxygen_rating = temp_o2_rating_0s;
+            }
+        }
+
+        if temp_co2_rating.len() > 1 {
+            let mut temp_co2_rating_1s = vec![];
+            let mut temp_co2_rating_0s = vec![];
+            for line in temp_co2_rating.iter() {
+                if line.chars().nth(index).unwrap() == '1' {
+                    temp_co2_rating_1s.push(line.to_string());
+                } else {
+                    temp_co2_rating_0s.push(line.to_string());
+                }
+            }
+
+            // Least common value
+            if temp_co2_rating_0s.len() <= temp_co2_rating_1s.len() {
+                // Keep values with 0 or least common value
+                temp_co2_rating = temp_co2_rating_0s;
+            } else {
+                temp_co2_rating = temp_co2_rating_1s;
+            }
+        }
+        index += 1;
+    }
+
+    convert_to_decimal(temp_oxygen_rating[0].to_string())
+        * convert_to_decimal(temp_co2_rating[0].to_string())
 }
 
 fn read_lines_as_str<P>(filename: P) -> Vec<String>
