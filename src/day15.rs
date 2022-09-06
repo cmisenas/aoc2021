dd9d39cb18d226fcc6644c028e8cb37f204a4b88
Jade Misenas <cjmisenas@gmail.com>
2022-09-05 21:12:11 -0700

    Day 15 part 1 done

--- src/day15.rs
+++ src/day15.rs
@@ -1,12 +1,11 @@
 use std::collections::HashMap;
-use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 pub fn main() {
     let lines = read_lines_as_str("./day15.input");
-    let paths: Vec<Vec<usize>> = lines
+    let grid: Vec<Vec<usize>> = lines
         .iter()
         .map(|l| {
             l.chars()
@@ -14,139 +13,83 @@ pub fn main() {
                 .collect::<Vec<usize>>()
         })
         .collect();
-    let answer1 = solve1(&paths);
-    let answer2 = solve2(&paths);
+    let answer1 = solve1(&grid);
     println!("Day 15 answers");
     println!("Answer 1 {}", answer1);
+
+    let answer2 = solve2(&grid);
     println!("Answer 2 {}", answer2);
 }
 
-fn solve1(paths: &[Vec<usize>]) -> usize {
-    let result = dfs(
-        (0, 0),
-        paths,
-        Vec::new(),
-        (paths[0].len() - 1, paths.len() - 1),
-    );
-    // let result = naive_navigate((0, 0), paths);
-    get_paths_score(&result, paths)
+fn solve1(grid: &[Vec<usize>]) -> usize {
+    // Assuming you can only go right or bottom
+    let curr = (0, 0);
+    let end = (grid.len() - 1, grid[0].len() - 1);
+
+    println!("Start is {}", grid[curr.0][curr.1]);
+    println!("End is {}", grid[end.0][end.1]);
+    calc_path(grid)
 }
 
 fn solve2(lines: &[Vec<usize>]) -> usize {
     0
 }
 
-fn naive_navigate(mut current_pt: (usize, usize), paths: &[Vec<usize>]) -> Vec<(usize, usize)> {
-    let mut potential_path: Vec<(usize, usize)> = Vec::new();
-    let mut end = (paths[0].len() - 1, paths.len() - 1);
-    let width = paths[0].len();
-    let height = paths.len();
+fn calc_path(grid: &[Vec<usize>]) -> usize {
+    let mut dist: HashMap<(usize, usize), usize> = HashMap::new();
+    let mut prev: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
+    let mut unvisited: HashMap<(usize, usize), usize> = HashMap::new();
+    for (i, row) in grid.iter().enumerate() {
+        for (j, col) in row.iter().enumerate() {
+            dist.insert((j, i), usize::MAX);
+            prev.insert((j, i), (usize::MAX, usize::MAX));
+            unvisited.insert((j, i), *col);
+        }
+    }
+    let curr = (0, 0);
+    let width = grid[0].len();
+    let height = grid.len();
+    let target = (width - 1, height - 1);
 
-    loop {
-        let neighbors = get_neighbors_simple(current_pt, width, height);
-        if neighbors.len() > 1 {
-            let mut neighbor1 = paths[neighbors[0].1][neighbors[0].0];
-            let mut neighbor2 = paths[neighbors[1].1][neighbors[1].0];
-            // Compare each one
-            if (neighbor1 as i8 - neighbor2 as i8).abs() <= 2 {
-                let best_coords1 = get_neighbors_simple(neighbors[0], width, height)
-                    .iter()
-                    .min_by(|coord1, coord2| {
-                        let pt1 = paths[coord1.1][coord1.0];
-                        let pt2 = paths[coord2.1][coord2.0];
-                        pt1.cmp(&pt2)
-                    })
-                    .unwrap()
-                    .clone();
-                let best_coords2 = get_neighbors_simple(neighbors[1], width, height)
-                    .iter()
-                    .min_by(|coord1, coord2| {
-                        let pt1 = paths[coord1.1][coord1.0];
-                        let pt2 = paths[coord2.1][coord2.0];
-                        pt1.cmp(&pt2)
-                    })
-                    .unwrap()
-                    .clone();
-                neighbor1 += paths[best_coords1.1][best_coords1.0];
-                neighbor2 += paths[best_coords2.1][best_coords2.0];
+    dist.entry(curr).and_modify(|d| *d = 0);
+    prev.entry(curr).and_modify(|p| *p = (0, 0));
+
+    while unvisited.len() > 0 {
+        // Get the vertex in unvisited that has min dist
+        let dist_copy = dist.clone();
+        let (u, dist_u) = dist_copy
+            .iter()
+            .filter(|(key, _)| unvisited.contains_key(key))
+            .min_by(|(_, x), (_, y)| x.cmp(y))
+            .unwrap();
+        // Remove that vertex in unvisited
+        unvisited.remove(u);
+
+        // Get neighbors of vertex
+        let neighbors = get_neighbors(*u, width, height);
+        for v in neighbors.iter() {
+            // Calculate the length of distance between neighbor and vertex
+            let alt = *dist_u + grid[v.1][v.0];
+            let v_dist = dist.get(v).unwrap();
+
+            if alt < *v_dist {
+                dist.entry(*v).and_modify(|d| *d = alt);
+                prev.entry(*v).and_modify(|p| *p = *u);
             }
-            current_pt = match neighbor1 < neighbor2 {
-                true => neighbors[0],
-                _ => neighbors[1],
-            };
-            println!(
-                "Comparing {:?} < {:?}? {:?} wins by {}",
-                neighbors[0],
-                neighbors[1],
-                current_pt,
-                (neighbor1 as i8 - neighbor2 as i8).abs()
-            );
-        } else {
-            current_pt = neighbors[0];
-            println!("Only 1 choice? {:?} wins", neighbors[0]);
-        }
-        potential_path.push(current_pt);
-
-        if current_pt == end {
-            break;
         }
     }
 
-    potential_path
+    *dist.get(&target).unwrap()
 }
 
-fn get_best_neighbor_score(coord: (usize, usize), paths: &[Vec<usize>]) -> usize {
-    0
-}
-
-fn dfs(
-    start: (usize, usize),
-    grid: &[Vec<usize>],
-    mut path: Vec<(usize, usize)>,
-    end: (usize, usize),
-) -> Vec<(usize, usize)> {
-    // Distance of 1 from the end
-    if (start.0 as i32 - end.0 as i32).abs() + (start.1 as i32 - end.1 as i32).abs() == 1 {
-        println!("Found end distance {:?}", start);
-        path.push(end);
-        return path;
-    }
-
-    let neighbors = get_neighbors_simple(start, grid[0].len(), grid[1].len());
-    let mut potential_paths: Vec<Vec<(usize, usize)>> = Vec::new();
-    for neighbor in neighbors.iter() {
-        let mut path_copy = path.clone();
-        path_copy.push(start);
-        let result = dfs(*neighbor, grid, path_copy, end);
-        potential_paths.push(result);
-    }
-
-    potential_paths
-        .iter()
-        .min_by(|path1, path2| get_paths_score(path1, grid).cmp(&get_paths_score(path2, grid)))
-        .unwrap()
-        .clone()
+fn enlarge(grid: &[Vec<usize>]) -> &[Vec<usize>] {
+    let enlarged_grid = grid.clone();
+    enlarged_grid
 }
 
 fn get_paths_score(path: &Vec<(usize, usize)>, grid: &[Vec<usize>]) -> usize {
     path.iter()
         .fold(0, |acc, coord| acc + grid[coord.1][coord.0])
-}
-
-fn get_neighbors_simple(point: (usize, usize), width: usize, height: usize) -> Vec<(usize, usize)> {
-    let mut neighbors = Vec::new();
-    // Right
-    if point.0 < width - 1 {
-        neighbors.push((point.0 + 1, point.1));
-    }
-
-    // Bottom
-    if point.1 < height - 1 {
-        // X
-        neighbors.push((point.0, point.1 + 1));
-    }
-
-    neighbors.clone()
 }
 
 fn get_neighbors(point: (usize, usize), width: usize, height: usize) -> Vec<(usize, usize)> {
@@ -163,34 +106,14 @@ fn get_neighbors(point: (usize, usize), width: usize, height: usize) -> Vec<(usi
 
     // Top
     if point.1 > 0 {
-        // Left
-        if point.0 > 0 {
-            neighbors.push((point.0 - 1, point.1 - 1));
-        }
-
         // X
         neighbors.push((point.0, point.1 - 1));
-
-        // Right
-        if point.0 < width - 1 {
-            neighbors.push((point.0 + 1, point.1 - 1));
-        }
     }
 
     // Bottom
     if point.1 < height - 1 {
-        // Left
-        if point.0 > 0 {
-            neighbors.push((point.0 - 1, point.1 + 1));
-        }
-
         // X
         neighbors.push((point.0, point.1 + 1));
-
-        // Right
-        if point.0 < width - 1 {
-            neighbors.push((point.0 + 1, point.1 + 1));
-        }
     }
 
     neighbors.clone()
