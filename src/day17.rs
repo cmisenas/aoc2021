bca843baa1808d420b15176e52b0e415aaaf3d9e
Jade Misenas <cjmisenas@gmail.com>
2022-09-11 18:10:35 -0700

    Day 17 done

--- src/day17.rs
+++ src/day17.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -25,25 +23,61 @@ pub fn main() {
         .collect::<Vec<isize>>();
     let x_range = (parse_x[0], parse_x[1]);
     let y_range = (parse_y[0], parse_y[1]);
-    println!("x range {:?}", x_range);
-    println!("y range {:?}", y_range);
-    let answer1 = solve1(x_range, y_range);
-    //let answer2 = solve2(&lines);
-    //println!("Day 17 answers");
-    //println!("Answer 1 {}", answer1);
-    //println!("Answer 2 {}", answer2);
+    let answer = solve(x_range, y_range);
+    println!("Day 17 answers");
+    println!("Answer 1 {}", answer.0);
+    println!("Answer 2 {}", answer.1);
 }
 
-fn solve1(x_range: (isize, isize), y_range: (isize, isize)) -> u32 {
-    0
+fn solve(x_range: (isize, isize), y_range: (isize, isize)) -> (isize, usize) {
+    let mut highest_y = 0;
+    let mut good_hits = 0;
+
+    for x in 1..(x_range.1 + 1) {
+        for y in y_range.0..((y_range.0 * -1) + 1) {
+            let highest_pos_y = within_range(x, y, x_range, y_range);
+            if highest_pos_y >= 0 {
+                good_hits = good_hits + 1;
+            }
+            if highest_pos_y > highest_y {
+                highest_y = highest_pos_y;
+            }
+        }
+    }
+    (highest_y, good_hits)
 }
 
-fn within_range(start: (isize, isize)) -> bool {
-    false
-}
+fn within_range(
+    vel_x: isize,
+    vel_y: isize,
+    x_range: (isize, isize),
+    y_range: (isize, isize),
+) -> isize {
+    let mut does_hit = -1;
+    let mut pos_x = 0;
+    let mut pos_y = 0;
+    let mut highest_y = pos_y;
+    let mut curr_x = vel_x;
+    let mut curr_y = vel_y;
+    let valid_x_range = x_range.0..(x_range.1 + 1);
+    let valid_y_range = y_range.0..(y_range.1 + 1);
+    while pos_x < x_range.1 && pos_y > y_range.0 {
+        pos_x = pos_x + curr_x;
+        pos_y = pos_y + curr_y;
+        if curr_x > 0 {
+            curr_x = curr_x - 1;
+        }
+        curr_y = curr_y - 1;
+        if pos_y > highest_y {
+            highest_y = pos_y;
+        }
 
-fn solve2(lines: &[u32]) -> u32 {
-    0
+        if valid_x_range.contains(&pos_x) && valid_y_range.contains(&pos_y) {
+            does_hit = highest_y;
+            break;
+        }
+    }
+    does_hit
 }
 
 fn read_lines_as_str<P>(filename: P) -> Vec<String>
